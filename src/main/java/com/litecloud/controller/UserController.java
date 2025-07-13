e37b5fffc7f26528c6a25c4555a5a64ec0b65544
Anyuersuper <lvzaosong@gmail.com>
2025-07-13 23:17:35 +0800

    roleRecognize
    鉴权

--- src/main/java/com/litecloud/controller/UserController.java
+++ src/main/java/com/litecloud/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.litecloud.controller;
 
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +23,16 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody Users user) {
-        return userService.login(user.getUsername(), user.getPassword());
+    public Map<String, Object> login(@RequestBody Users user, HttpServletRequest request) {
+        Map<String, Object> res = userService.login(user.getUsername(), user.getPassword());
+        if ("success".equals(res.get("status"))) {
+            // 登录成功后写入Session
+            request.getSession().setAttribute("userId", res.get("userId"));
+            // 设置Spring Security认证
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(), null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        return res;
     }
-   
 }
